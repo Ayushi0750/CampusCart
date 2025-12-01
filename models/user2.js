1b443a9f26bf9329c9dd150e75e2112343e71d66
Ayushi0750 <tayushi297@gmail.com>
2025-12-01 11:49:33 +0530

    Modify user2Schema to relax field requirements
    Removed required constraints from username and userType fields in user2Schema.

--- models/user2.js
+++ models/user2.js
@@ -1,4 +1,8 @@
+
+
+
 //user schema for events
+
 
 
 const mongoose = require("mongoose");
@@ -6,14 +10,11 @@ const passportLocalMongoose = require("passport-local-mongoose");
 
 const user2Schema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true },
-  userType: {
-    type: String,
-    enum: ["campus", "event"],
-    required: true
-  }
-}, { timestamps: true });
+  username: { type: String }
+});
+
 
 user2Schema.plugin(passportLocalMongoose, { usernameField: "email" });
+
 
 module.exports = (conn) => conn.model("User2", user2Schema);
