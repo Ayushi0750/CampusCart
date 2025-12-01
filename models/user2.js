43440a8b0550416fa00cad86942ad85dbb3ac59c
Ayushi0750 <tayushi297@gmail.com>
2025-12-01 11:18:04 +0530

    Add userType field and require username in user2 schema

--- models/user2.js
+++ models/user2.js
@@ -1,17 +1,19 @@
 //user schema for events
 
 
-
 const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const user2Schema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  username: { type: String }
-});
-
+  username: { type: String, required: true },
+  userType: {
+    type: String,
+    enum: ["campus", "event"],
+    required: true
+  }
+}, { timestamps: true });
 
 user2Schema.plugin(passportLocalMongoose, { usernameField: "email" });
-
 
 module.exports = (conn) => conn.model("User2", user2Schema);
