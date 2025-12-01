632d421d33d2e2ad279db32c04e99c9b92b21e03
Ayushi0750 <tayushi297@gmail.com>
2025-12-01 11:48:55 +0530

    Refactor user schema in models/user.js
    Refactor user schema by cleaning up code and ensuring unique email field.

--- models/user.js
+++ models/user.js
@@ -1,14 +1,15 @@
 
 
 
-// models/user.js
-
-const mongoose = require("mongoose");
+//user schema for products
+const mongoose= require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
+
+const passportLocalMongoose= require("passport-local-mongoose");
+
 
 const userSchema = new Schema({
-  username: {
+ username: {
     type: String,
     required: true,
     trim: true,
@@ -17,21 +18,19 @@ const userSchema = new Schema({
   email: {
     type: String,
     required: true,
-    unique: true
-  },
-  userType: {
-    type: String,
-    enum: ["campus", "event"],   
-    required: true
+    unique:true,
+    
   }
-}, { timestamps: true });
+}, {
+  timestamps: true
+});
 
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: "email"
+});
 
-userSchema.plugin(passportLocalMongoose, { usernameField: "email" });
 
 const User = mongoose.model("User", userSchema);
-module.exports = User;
-
-const User = mongoose.model("User", userSchema);
 
 module.exports = User;
+
